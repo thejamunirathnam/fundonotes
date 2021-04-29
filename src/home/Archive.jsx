f7ba0be3edd29a2414f5b373ab0ffccdee9eba4c
unknown <thejasree1996@gmail.com>
2021-04-29 19:36:32 +0530

    3 rd commit

--- src/home/Archive.jsx
+++ src/home/Archive.jsx
@@ -33,7 +33,7 @@ export class Archive extends Component {
         })
       }
     }).catch((error) => {
-      console.log(error); // error msg displaying
+      console.log(error); // error msg displaying is 3 rd commit
     })
   }
 
